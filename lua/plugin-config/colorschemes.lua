d5a444ea30a7584eefdafa43e2127797f58995cc
Zheng <cheneyzheng@yeah.net>
2023-09-01 01:12:11 +0800

    初步增加lualine，nerd-tree，以及增加三个主题

---
+++ lua/plugin-config/colorschemes.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = true,
+    },
+    {
+        "sainnhe/gruvbox-material",
+        lazy = true,
+    }
+}
